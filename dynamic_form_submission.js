e63f19adaba54cd3c5f58e4ae9557db2577e465c
Cosmin A <cosmin@wishpond.com>
2013-08-19 17:01:02 -0700

    removed whitespace errors

--- dynamic_form_submission.js
+++ dynamic_form_submission.js
@@ -4,59 +4,59 @@
 ;(function($, doc, win) {
   "use strict";
 
-	$.DFS_Form = {
-		BUTTON_REL : "data-dfs-register-button",
-		ID: "DynamicFormSubmissionsHandler",
-		SELECTOR: function() {
-			return "#" + this.ID;	
-		},
-		name : "",
-		// these are fields that will be inserted into a virtual form dynamically.
-		fields: [],
-		init: function ( method, action ) {
-			this.fields = [];
-			this.method = method;
-			this.action = action;
-			$("body").append( '<form id="' + this.ID + '"\
-												action="' + action + '"\
-												method="' + method + '"\
-												style="display:none; visibility:hidden; height:0px; width:0px;">\
-										</form>' );
-		},
-		register_button: function( button, callback, delay_submit ){
-			button.on("click", function(e){
-				if ( typeof callback !== "undefined" ) {
-					callback();
-				}
+  $.DFS_Form = {
+    BUTTON_REL : "data-dfs-register-button",
+    ID: "DynamicFormSubmissionsHandler",
+    SELECTOR: function() {
+      return "#" + this.ID; 
+    },
+    name : "",
+    // these are fields that will be inserted into a virtual form dynamically.
+    fields: [],
+    init: function ( method, action ) {
+      this.fields = [];
+      this.method = method;
+      this.action = action;
+      $("body").append( '<form id="' + this.ID + '"\
+                        action="' + action + '"\
+                        method="' + method + '"\
+                        style="display:none; visibility:hidden; height:0px; width:0px;">\
+                    </form>' );
+    },
+    register_button: function( button, callback, delay_submit ){
+      button.on("click", function(e){
+        if ( typeof callback !== "undefined" ) {
+          callback();
+        }
         if( typeof delay_submit === "undefined" ) {
           $.DFS_Form.submit();
         }
 
         //preventDefault and stopPropagation
-				return false;
-			});
-		},
-		add_field: function( name, value ) {
-			var pos = this.fields.indexOf( name );
+        return false;
+      });
+    },
+    add_field: function( name, value ) {
+      var pos = this.fields.indexOf( name );
 
-			// remove existing fields with same name
-			if ( pos >= 0 ) {
-				this.fields.splice( name, fields.length );
-			}
+      // remove existing fields with same name
+      if ( pos >= 0 ) {
+        this.fields.splice( name, fields.length );
+      }
 
-			this.fields[ name ] = value;
-		},
-		submit: function() {
-			var fields_string = "";
-			for( var name in this.fields ) {
-				fields_string += '<input type="hidden" name="' + name + '" value="' + this.fields[ name ] + '" />';
-			}
-			$( this.SELECTOR() ).html( fields_string );
-		}
-	};
+      this.fields[ name ] = value;
+    },
+    submit: function() {
+      var fields_string = "";
+      for( var name in this.fields ) {
+        fields_string += '<input type="hidden" name="' + name + '" value="' + this.fields[ name ] + '" />';
+      }
+      $( this.SELECTOR() ).html( fields_string );
+    }
+  };
 
-	$( "button[" + $.DFS_Form.BUTTON_REL + "]" ).each(function() {
-		$.DFS_Form.register_button( $(this) );
-	});
+  $( "button[" + $.DFS_Form.BUTTON_REL + "]" ).each(function() {
+    $.DFS_Form.register_button( $(this) );
+  });
 })(jQuery, document, window);
 //$.DFS_Form.init( "POST", "<address-to-submit-data>");
